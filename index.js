51bbb5c6edffdcc48bc5a96747dd26d95d4517d2
Chetan Janardhana <janachet@amazon.com>
2024-10-27 01:17:48 +0000

    working plain docker, no compose, simple nodejs application

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const redis = require('redis');
+//const redis = require('redis');
 
 const app = express();
-const redis_client = redis.createClient({
-    host: "redis-server",
-    port: 6379
-});
-redis_client.set('visits', 0);
+// const redis_client = redis.createClient({
+//     host: "redis-server",
+//     port: 6379
+// });
+// redis_client.set('visits', 0);
+
+// app.get(
+//     '/',
+//     (req, res) => {
+//         redis_client.get('visits', (err, visits)=> {
+//             res.send('Hi There no of visits is:' + visits);
+//             redis_client.set('visits', parseInt(visits) + 1);
+//         });
+//     }
+// );
 
 app.get(
     '/',
     (req, res) => {
-        redis_client.get('visits', (err, visits)=> {
-            res.send('Hi There no of visits is:' + visits);
-            redis_client.set('visits', parseInt(visits) + 1);
-        });
+
+        res.send('Hi There');
+
     }
 );
 
