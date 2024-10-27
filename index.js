cec44f0137bf920cb8e8a86ef7f8f4a612c13ec5
Chetan Janardhana <janachet@amazon.com>
2024-10-27 01:00:15 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const redis_client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+redis_client.set('visits', 0);
+
+app.get(
+    '/',
+    (req, res) => {
+        redis_client.get('visits', (err, visits)=> {
+            res.send('Hi There no of visits is:' + visits);
+            redis_client.set('visits', parseInt(visits) + 1);
+        });
+    }
+);
+
+app.listen(
+    8080,
+    () => {
+        console.log('Listening on port 8080');
+    }
+);
