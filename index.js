816e0e30c9e93c31c9f43269a64c32053f5a415d
Chetan Janardhana <janachet@amazon.com>
2024-10-27 01:31:02 +0000

    working with docker compose including redis, visits app

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const express = require('express');
-//const redis = require('redis');
+const redis = require('redis');
 
 const app = express();
-// const redis_client = redis.createClient({
-//     host: "redis-server",
-//     port: 6379
-// });
-// redis_client.set('visits', 0);
-
-// app.get(
-//     '/',
-//     (req, res) => {
-//         redis_client.get('visits', (err, visits)=> {
-//             res.send('Hi There no of visits is:' + visits);
-//             redis_client.set('visits', parseInt(visits) + 1);
-//         });
-//     }
-// );
+const redis_client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+redis_client.set('visits', 0);
 
 app.get(
     '/',
     (req, res) => {
-
-        res.send('Hi There');
-
+        redis_client.get('visits', (err, visits)=> {
+            res.send('Hi There no of visits is:' + visits);
+            redis_client.set('visits', parseInt(visits) + 1);
+        });
     }
 );
 
