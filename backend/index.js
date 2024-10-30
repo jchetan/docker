c56a6e251db60adca2e8052ba49f0c8e775930f1
Chetan Janardhana <janachet@amazon.com>
2024-10-30 17:51:52 +0000

    working till section section 9 end

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const app = express();
 const redis = require('redis');
-const process = require('process');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
-app.use(cors);
-app.use(bodyParser.json);
+app.use(cors());
+
+app.use(bodyParser.json());
 const redis_client = redis.createClient({
     host: "redis",
     port: 6379
@@ -16,31 +16,14 @@ redis_client.set('visits', 0);
 app.get(
     '/api',
     (req, res) => {
-        try {
-            const workouts = {
-                name: "reps",
-                id: 2
-            };
-            res.status(200).json(workouts);
-        } catch (error) {
-            res.status(400).json({ error: "error happened" });
-        }
-    }
-);
-
-app.post(
-    '/value',
-    async (req, res) => {
-        const key = req.body.key;
-        const value = req.body.value;
-        await redis_client.set(key, value);
-        res.send('Success');
+        console.log("Inside get")
+        res.send("Hi Geetha and Chetan");
     }
 );
 
 app.listen(
-    5000,
+    7000,
     () => {
-        console.log('Listening on port 5000');
+        console.log('Listening on port 7000');
     }
 );
