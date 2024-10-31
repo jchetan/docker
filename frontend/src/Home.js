186a11cbd3b8d3deda9662ff39a6fd2d8e2d5116
Chetan Janardhana <janachet@amazon.com>
2024-10-31 09:59:21 +0000

    front shows all keys and add key implemented

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,27 +2,44 @@ import { useState, useEffect } from 'react'
 
 const Home = () => {
 
-  useEffect(    
+  const [keys, setKeys] = useState([]);
+
+  useEffect(
     () => {
-      const fetchWorkouts = async () => {
-        console.log("json");
-        const response = await fetch('/api');
+      const fetchKeys = async () => {
+        const response = await fetch('/api/getkeys');
         const json = await response.json();
-        if (response.ok) {          
-          console.log(json);
+        if (response.ok) {
+          setKeys(json.keys);
+          console.log(json.keys);
         }
       }
-      fetchWorkouts();
+      fetchKeys();
     },
     []
   );
 
   return (
     <div className='container'>
+      Hello world!
+      {keys && keys.map((key1) => {
+        return (
+          <div key={key1} className="row justify-content-center" >
+            <div className="card" style={{ width: "25rem" }}>
+              <div className="card-body">
+                <h5 className="card-title">Key: {key1}</h5>                
+              </div>
+            </div>
+          </div>
+        )
 
-      Hello World Dear Chetan J!
+      })}
     </div>
   )
+
+
+
+
 }
 
 export default Home
