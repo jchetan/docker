610e791538af820b9197cc8423a0e90c7b70ccf1
Chetan Janardhana <janachet@amazon.com>
2024-10-31 09:32:11 +0000

    backend nodemon, apis get all keys, gey value for 1 key, add key value implemented

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,50 @@ const redis_client = redis.createClient({
     host: "redis",
     port: 6379
 });
-redis_client.set('visits', 0);
 
+//get all keys
 app.get(
     '/api',
     (req, res) => {
-        console.log("Inside get")
-        res.send("Hi Geetha and Chetan");
+        redis_client.keys('*', (err, keys) => {
+            if (err) throw err;
+            console.log("Inside get");            
+            res.status(200).json({keys});            
+        });
+    }
+);
+
+//get value for a key
+app.get(
+    '/api/:key',
+    async (req, res) => {
+        const { key } = req.params;
+        try {
+            redis_client.get(key, (err, value) => {
+                console.log(err);
+                const item = {
+                    "key": key,
+                    "value": value
+                }
+                res.status(200).json(item);
+            })
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+);
+
+// set key and value
+app.post(
+    '/api',
+    async (req, res) => {        
+        const { key, value } = req.body;
+        try {
+            redis_client.set(key, value);
+            res.status(200).json(req.body);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
 );
 
